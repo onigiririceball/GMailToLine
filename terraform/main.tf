b6ce209dacee2b457061a9a8b48b402a8b437a66
Yurika Tsukamoto <fdtgcirgkydj387@icloud.com>
2025-11-30 11:41:41 +0900

    feat: 1 terraformFormat

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,19 @@
 terraform {
-	required_providers{
-		google = {
-			source = "hashicorp/google"
-			version = "6.8.0"
-		}
-	}
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
 }
 
-provider  "google" {
-	project = "grounded-region-477206-b6"
-	region  = "asia-northeast1"
-	zone    = "asia-northeast1"
+provider "google" {
+  project = "grounded-region-477206-b6"
+  region  = "asia-northeast1"
+  zone    = "asia-northeast1"
 }
 
 resource "google_compute_network" "vpc_network" {
-	name = "terraform-network"
+  name = "terraform-network"
 }
- 
+
